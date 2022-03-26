d82e7603390a81b976ecff0593da6e487f3fb13b
Potato <theflash1205@gmail.com>
2022-03-25 21:13:57 -0400

    world and basic ball
    balls man

--- Scenes/BallScript.gd
+++ Scenes/BallScript.gd
@@ -4,8 +4,7 @@ extends CollisionShape2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +13,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _process(delta):
+	pass
